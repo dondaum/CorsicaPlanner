fef9cea2daf8254db4376dec68c2fda5db41f1c4
dondaum <sebastian.daum89@gmail.com>
2017-08-26 21:05:58 +0200

    changend default email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,5 @@
 class ReservationMailer < ApplicationMailer
-   default from: "from@example.com"
+   default from: "sebastian.daum89@gmail.com"
 
    def request_email(reservation)
     @reservation = reservation
